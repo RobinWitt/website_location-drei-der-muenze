506efa321c31d2f788f0ffec49bd116dbdcf8bb6
RobinWitt <reg.rw@posteo.de>
2023-05-12 15:37:36 +0200

    add telegram logo

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import InstagramLogo from "../Logos/Instagram";
+import TelegramLogo from "../Logos/Telegram";
 
 export default function Footer() {
   return (
     <footer>
-      <Link href={"/"}>
-        <InstagramLogo />
-      </Link>
+      <div className="container-links">
+        <Link href={"/"}>
+          <InstagramLogo />
+        </Link>
+        <Link href={"/"}>
+          <TelegramLogo />
+        </Link>
+      </div>
       <div className="container-links">
         <Link href={"/"}>Impressum</Link>
         <Link href={"/"}>Datenschutz</Link>
